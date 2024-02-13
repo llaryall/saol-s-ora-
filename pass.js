fb39c46e24030c5af22a4d36848493c38ab454c6
Arya <78265295+llaryall@users.noreply.github.com>
2024-02-14 01:14:19 +0330

    Add files via upload

---
+++ pass.js
@@ -0,0 +1,11 @@
+const input = document.getElementById("inpt");
+const button = document.getElementById("btn");
+
+function check() {
+  if (input.value === "saol síoraí❤️") {
+    alert("to my saol síoraí❤️");
+    window.location.href = "test.html";
+  } else {
+    alert("come on babe try again");
+  }
+}
